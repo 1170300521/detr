5f26b33d2bf61700d71ec1cc247c9b08c05cc75e
1170300521 <hitzhangyabo2017@163.com>
2021-05-27 10:22:58 +0800

    decrease checkpoint step

--- main.py
+++ main.py
@@ -234,7 +234,7 @@ def main(args):
         if args.output_dir:
             checkpoint_paths = [osp.join(output_dir,  'checkpoint.pth')]
             # extra checkpoint before LR drop and every 10 epochs
-            if (epoch + 1) % args.lr_drop == 0 or (epoch + 1) % 10 == 0:
+            if (epoch + 1) % args.lr_drop == 0 or (epoch + 1) % 5 == 0:
                 checkpoint_paths.append(osp.join(output_dir ,f'checkpoint{epoch:04}.pth'))
             for checkpoint_path in checkpoint_paths:
                 utils.save_on_master({
